7b11326bf7b9dfee4c6dcb5548f7c23ac68bec65
jnielek <jan.nielek@axbit.com>
2018-03-28 17:38:28 +0200

    Add project files.

---
+++ KoprzyTemp/KoprzyTemp.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		KoprzyTemp.ino
+ Created:	3/28/2018 5:38:22 PM
+ Author:	janni
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
